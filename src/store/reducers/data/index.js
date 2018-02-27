5f1f2c7ad043fab75d4f5238812835a35da5688f
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-02-27 18:38:18 +0100

    :sparkles: List organizer organizations

--- src/store/reducers/data/index.js
+++ src/store/reducers/data/index.js
@@ -3,6 +3,7 @@ import { keyValue } from 'k-ramel'
 export default {
   users: keyValue({ key: 'uid' }),
   events: keyValue({ key: 'id' }),
+  organizations: keyValue({ key: 'id' }),
   talks: keyValue({ key: 'id' }),
   proposals: keyValue({ key: 'id' }),
   ratings: keyValue({ key: 'uid' }),
